163cd903cf917bc01c49c76336355d9b4bad8012
Cesare Ferrari <cesare@maryandferrari.com>
2019-05-22 21:55:58 -0400

    Create PostContainer instances

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import dummyData from './dummy-data';
+import PostContainer from './components/PostContainer/PostContainer';
 
 function App() {
   return (
     <div className="App">
     <h1>Instagram clone</h1>
+      {dummyData.map(user => <PostContainer user={user} />)}
     </div>
   );
 }
---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostContainer = props => {
+  return(
+    <div>
+      <h2>{props.user.username}</h2>
+    </div>
+  )
+}
+
+export default PostContainer;
