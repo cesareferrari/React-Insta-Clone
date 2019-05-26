ddb1ffeea4741212c27439cb2d572428cea81a1b
Cesare Ferrari <cesare@maryandferrari.com>
2019-05-26 09:23:29 -0400

    Add search box

--- instagram-app/src/components/Header/Header.js
+++ instagram-app/src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
+import SearchBar from '../SearchBar/SearchBar';
 
 const Header = props => {
   return(
     <header>
       <h1>Instagram</h1>
+      <SearchBar />
     </header>
   )
 }
---
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBar = props => {
+  return(
+    <form>
+      <input name="search" value="" type="text" placeholder="Search" />
+    </form>
+  )
+}
+
+export default SearchBar;
